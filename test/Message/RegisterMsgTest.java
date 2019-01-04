0784ea038073b0dabc9ddc9048a9c0159b9aaded
DUBOCZKY, ADAM <T7073950@live.tees.ac.uk>
2019-01-04 17:24:02 +0000

    Added unit tests for RegisterMsg

---
+++ test/Message/RegisterMsgTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Message;
+
+import Agents.MetaAgent;
+import Agents.UserAgent;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dub_2
+ */
+public class RegisterMsgTest
+{
+    
+    /**
+     * Test of getAgent method, of class RegisterMsg.
+     */
+    @Test
+    public void testGetAgent()
+    {
+        System.out.println("Testing the method getAgent");
+        MetaAgent adam = new UserAgent("Adam", null);
+        RegisterMsg instance = new RegisterMsg(adam);
+        MetaAgent expResult = adam;
+        MetaAgent result = instance.getAgent();
+        System.out.println(result.getName());
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getHopCount method, of class RegisterMsg.
+     */
+    @Test
+    public void testGetHopCount()
+    {
+        System.out.println("Testing the method getHopCount");
+        MetaAgent adam = new UserAgent("Adam", null);
+        RegisterMsg instance = new RegisterMsg(adam); 
+        int expResult = 1;
+        int result = instance.getHopCount();
+        System.out.println(result);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getLastAgent method, of class RegisterMsg.
+     */
+    @Test
+    public void testGetLastAgent()
+    {
+        System.out.println("Testing the method getLastAgent");
+        MetaAgent adam = new UserAgent("Adam", null);
+        RegisterMsg instance = new RegisterMsg(adam);
+        String expResult = "Adam";
+        String result = instance.getLastAgent();
+        System.out.println(result);        
+        assertEquals(expResult, result);
+    }
+    
+}
